b3ffa90d51052c9ea3d82ad299e07d117bf7eb22
Jenkins <cforall@plg.uwaterloo.ca>
2023-06-30 00:16:21 -0400

    Push from build machine: cfa-cc-1.0.0.2548

--- src/AST/LinkageSpec.hpp
+++ src/AST/LinkageSpec.hpp
@@ -24,56 +24,57 @@ namespace ast {
 
 namespace Linkage {
 
-	/// Bitflags for linkage specifiers
-	enum {
-		Mangle       = 1 << 0,
-		Generate     = 1 << 1,
-		Overrideable = 1 << 2,
-		Builtin      = 1 << 3,
-		GccBuiltin   = 1 << 4
-	};
+/// Bitflags for linkage specifiers
+enum {
+	Mangle       = 1 << 0,
+	Generate     = 1 << 1,
+	Overrideable = 1 << 2,
+	Builtin      = 1 << 3,
+	GccBuiltin   = 1 << 4
+};
 
-	/// Bitflag type for storage classes
-	struct spec_flags {
-		union {
-			unsigned int val;
-			struct {
-				bool is_mangled      : 1;
-				bool is_generatable  : 1;
-				bool is_overrideable : 1;
-				bool is_builtin      : 1;
-				bool is_gcc_builtin  : 1;
-			};
+/// Bitflag type for storage classes
+struct spec_flags {
+	union {
+		unsigned int val;
+		struct {
+			bool is_mangled      : 1;
+			bool is_generatable  : 1;
+			bool is_overrideable : 1;
+			bool is_builtin      : 1;
+			bool is_gcc_builtin  : 1;
 		};
-
-		constexpr spec_flags( unsigned int val ) : val(val) {}
 	};
 
-	using Spec = bitfield<spec_flags>;
+	constexpr spec_flags( unsigned int val ) : val(val) {}
+};
 
-	/// If `cmd` = "C" returns `spec` with `is_mangled = false`.
-	/// If `cmd` = "Cforall" returns `spec` with `is_mangled = true`.
-	Spec update( CodeLocation loc, Spec spec, const std::string * cmd );
+using Spec = bitfield<spec_flags>;
 
-	/// A human-readable name for this spec
-	std::string name( Spec spec );
+/// If `cmd` = "C" returns `spec` with `is_mangled = false`.
+/// If `cmd` = "Cforall" returns `spec` with `is_mangled = true`.
+Spec update( CodeLocation loc, Spec spec, const std::string * cmd );
 
-	// Pre-defined flag combinations
+/// A human-readable name for this spec
+std::string name( Spec spec );
 
-	/// C built-in defined in prelude
-	constexpr Spec Intrinsic  = { Mangle | Generate | Overrideable | Builtin };
-	/// Ordinary Cforall
-	constexpr Spec Cforall    = { Mangle | Generate };
-	/// C code: not overloadable, not mangled
-	constexpr Spec C          = { Generate };
-	/// Built by translator (e.g. struct assignment)
-	constexpr Spec AutoGen    = { Mangle | Generate | Overrideable };
-	/// GCC internal
-	constexpr Spec Compiler   = { Mangle | Builtin | GccBuiltin };
-	/// Mangled builtins
-	constexpr Spec BuiltinCFA = { Mangle | Generate | Builtin };
-	/// Non-mangled builtins
-	constexpr Spec BuiltinC   = { Generate | Builtin };
+// Pre-defined flag combinations
+
+/// C built-in defined in prelude
+constexpr Spec Intrinsic  = { Mangle | Generate | Overrideable | Builtin };
+/// Ordinary Cforall
+constexpr Spec Cforall    = { Mangle | Generate };
+/// C code: not overloadable, not mangled
+constexpr Spec C          = { Generate };
+/// Built by translator (e.g. struct assignment)
+constexpr Spec AutoGen    = { Mangle | Generate | Overrideable };
+/// GCC internal
+constexpr Spec Compiler   = { Mangle | Builtin | GccBuiltin };
+/// Mangled builtins
+constexpr Spec BuiltinCFA = { Mangle | Generate | Builtin };
+/// Non-mangled builtins
+constexpr Spec BuiltinC   = { Generate | Builtin };
+
 }
 
 }
