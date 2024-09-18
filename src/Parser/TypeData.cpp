8ea148f92aa19ff0cd84352223dd28c35db5d93a
Jenkins <cforall@plg.uwaterloo.ca>
2024-09-18 00:07:21 -0400

    Push from build machine: cfa-cc-1.0.0.3035

--- src/Parser/TypeData.cpp
+++ src/Parser/TypeData.cpp
@@ -1485,8 +1485,8 @@ ast::EnumDecl * buildEnum(
 		} else if ( cur->has_enumeratorValue() ) {
 			ast::Expr * initValue;
 			if ( ret->isCfa && ret->base ) {
-				initValue = new ast::CastExpr( cur->enumeratorValue->location, maybeMoveBuild( cur->consume_enumeratorValue() ),
-											   ret->base );
+				CodeLocation location = cur->enumeratorValue->location;
+				initValue = new ast::CastExpr( location, maybeMoveBuild( cur->consume_enumeratorValue() ), ret->base );
 			} else {
 				initValue = maybeMoveBuild( cur->consume_enumeratorValue() );
 			}
